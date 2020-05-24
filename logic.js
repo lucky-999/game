babc718685b3ebd9c99f9550edbad9a87c42a26d
Ilya Tatarinov <itatarinov7@gmail.com>
2020-05-24 10:27:52 +0300

    base win logic

--- logic.js
+++ logic.js
@@ -22,42 +22,42 @@ function nextPlayer() {
   activePlayer = +!activePlayer;
 }
 
-function checkWinner() {
-  let i;
-  let j;
-  for (i = 0; i < playBoard.length; i ++) {
-    for (j = 0; j < playBoard[i].length; j ++) {
-      if (playBoard[i][j] === players[activePlayer] && playBoard[i][j + 1] === players[activePlayer] && playBoard[i][j + 1 + 1] === players[activePlayer]) {
-        showWinner(activePlayer);
-      } else if (playBoard[j][i] === players[activePlayer] && playBoard[j + 1][i] === players[activePlayer]){
-        console.log('есть победитель');
-      }
-    }
-  }
-}
-
 // function checkWinner() {
-//   if (((playBoard[0][0] === players[activePlayer]) && (playBoard[0][1] === players[activePlayer]) && (playBoard[0][2] === players[activePlayer])) || 
-
-//   ((playBoard[1][0] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[1][2] === players[activePlayer])) || 
-
-//   ((playBoard[2][0] === players[activePlayer]) && (playBoard[2][1] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
-
-//   ((playBoard[0][0] === players[activePlayer]) && (playBoard[1][0] === players[activePlayer]) && (playBoard[2][0] === players[activePlayer])) || 
-
-//   ((playBoard[0][1] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][1] === players[activePlayer])) || 
-
-//   ((playBoard[0][2] === players[activePlayer]) && (playBoard[1][2] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
-
-//   ((playBoard[0][0] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
-
-//   ((playBoard[0][2] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][0] === players[activePlayer]))) {
-    
-//      showWinner(activePlayer);
-//   } else {
-//       nextPlayer();
+//   let i;
+//   let j;
+//   for (i = 0; i < playBoard.length; i ++) {
+//     for (j = 0; j < playBoard[i].length; j ++) {
+//       if (playBoard[i][j] === players[activePlayer] && playBoard[i][j + 1] === players[activePlayer] && playBoard[i][j + 1 + 1] === players[activePlayer]) {
+//         showWinner(activePlayer);
+//       } else if (playBoard[j][i] === players[activePlayer] && playBoard[j + 1][i] === players[activePlayer]){
+//         console.log('есть победитель');
+//       }
+//     }
 //   }
 // }
+
+function checkWinner() {
+  if (((playBoard[0][0] === players[activePlayer]) && (playBoard[0][1] === players[activePlayer]) && (playBoard[0][2] === players[activePlayer])) || 
+
+  ((playBoard[1][0] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[1][2] === players[activePlayer])) || 
+
+  ((playBoard[2][0] === players[activePlayer]) && (playBoard[2][1] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
+
+  ((playBoard[0][0] === players[activePlayer]) && (playBoard[1][0] === players[activePlayer]) && (playBoard[2][0] === players[activePlayer])) || 
+
+  ((playBoard[0][1] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][1] === players[activePlayer])) || 
+
+  ((playBoard[0][2] === players[activePlayer]) && (playBoard[1][2] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
+
+  ((playBoard[0][0] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][2] === players[activePlayer])) || 
+
+  ((playBoard[0][2] === players[activePlayer]) && (playBoard[1][1] === players[activePlayer]) && (playBoard[2][0] === players[activePlayer]))) {
+    
+     showWinner(activePlayer);
+  } else {
+      nextPlayer();
+  }
+}
   
 
 function click(row, col) {
